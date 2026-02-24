28de52a5c645542bb779aa85f0cc020a8f8dae74
Kritika Das <94691076+kritika-das@users.noreply.github.com>
2026-02-24 20:55:34 +0530

    Configure for GitHub Pages deployment and rename app to My Tasks
    - Update homepage URL in package.json to match repository name
    - Fix index.html to use relative paths for assets (fixes white page issue)
    - Change app title from Taskify to My Tasks
    - Remove references to missing manifest and logo files

--- src/App.tsx
+++ src/App.tsx
@@ -98,7 +98,7 @@ const App: React.FC = () => {
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="App">
-        <span className="heading">Taskify</span>
+        <span className="heading">My Tasks</span>
         <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
         <TodoList
           todos={todos}
